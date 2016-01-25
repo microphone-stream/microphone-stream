7ee3d84c3fcf846303e69ad8ded5f99d7c3ca04d
Nathan Friedly <nathan@nfriedly.com>
2016-01-25 13:15:36 -0500

    added a format event test

--- test/spec.js
+++ test/spec.js
@@ -41,6 +41,27 @@ describe("MicrophoneStream", function() {
     });
   });
 
+  it("should emit a format event", function(done) {
+    getUserMedia(function(err, stream) {
+      if (err) {
+        return done(err);
+      }
+
+      var micStream = new MicrophoneStream(stream);
+      micStream.on('error', done)
+        .on('format', function(format) {
+          assert.deepEqual(format, {
+            channels: 1,
+            bitDepth: 32,
+            sampleRate: 48000,
+            signed: true,
+            float: true
+          });
+          done();
+        });
+    });
+  });
+
   describe("toRaw", function() {
     it("should convert fro a buffer to a Float32Array without copying data", function() {
       var source = new Buffer([0,0,0x80,0x3f]);
