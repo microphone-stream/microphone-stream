3489e2fbb7534635295b70fc0b8b166992054d4c
Nathan Friedly <nathan@nfriedly.com>
2021-02-04 10:59:58 -0500

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
-  'extends': 'google',
+  "plugins": ["prettier"],
+  'extends': ['google', 'prettier'],
   'env': {
     'browser': true,
     'node': true
   },
   'rules': {
+    "prettier/prettier": ["error"],
     'max-len': 'off',
     'no-inline-comments': 'off',
     'no-var': 'off',
